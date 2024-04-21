f58350727a8aed8d8d6beed58b475d58f5bccb20
Gacha <jlgacha75@gmail.com>
2024-04-21 20:20:25 +0200

    Iniciada pagina de Inicio

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+    return (
+        <div className="flex h-full justify-center items-center flex-col text-center">
+            <h1 className="text-[10vw] font-extrabold text-yellow-400">404</h1>
+            <h2 className="font-bold">404 - Not found!</h2>
+            <span className="max-w-prose mx-auto">
+                Es posible que la página que estás buscando haya sido eliminada, haya cambiado su
+                nombre o no esté disponible temporalmente.
+            </span>
+            <div className="mt-8 bg-[#53924c] text-white p-3 rounded-xl font-bold">
+                <Link href="/">Ir a la pagina de Inicio</Link>
+            </div>
+        </div>
+    );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,30 @@
+import Image from 'next/image';
+import image1 from '../../public/carousel/image3.webp';
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            Paco perez
+        <main>
+            <div className="flex justify-center flex-col bg-[--cartel] p-8 gap-5">
+                <div className="flex flex-col items-center justify-center">
+                    <h2 className="text-white font-bold text-md 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:lg">
+                        PRUEBA NUESTRO NUEVO
+                    </h2>
+                    <h2 className="text-yellow-400 font-bold text-2xl 2xl:text-6xl xl:text-6xl lg:text-5xl md:text-4xl sm:3xl">
+                        BOCADILLO DE TERNERA
+                    </h2>
+                </div>
+                <div className="flex justify-center">
+                    <Image src={image1} alt="Imagen de sandwich de Pollo" className="w-1/2" />
+                </div>
+                <span className="text-center text-neutral-300 text-[10px]">
+                    Campaña disponible solo hasta el 1 de Julio de 2024
+                </span>
+            </div>
+            <div className="flex justify-center items-center mt-4">
+                <h2 className="font-bold text-md 2xl:text-4xl xl:text-3xl lg:text-2xl py-3 px-16 md:text-xl sm:lg border-b border-yellow-400">
+                    Productos destacados
+                </h2>
+            </div>
         </main>
     );
 }
