2d86e466c4278505ac93579f15a3dd4f9e28d92a
Gacha <jlgacha75@gmail.com>
2024-05-04 15:54:23 +0200

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,14 +6,14 @@ import campero from '../../public/campero.webp';
 
 export default function Home() {
     return (
-        <>
+        <div className="heightDefault">
             <main>
                 <div className="flex justify-center flex-col bg-[--cartel] p-8 gap-5">
                     <div className="flex flex-col items-center justify-center">
-                        <h2 className="text-white font-bold text-md 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:lg">
+                        <h2 className="text-white font-bold text-md 2xl:text-4xl xl:text-3xl lg:text-2xl">
                             PRUEBA NUESTRO NUEVO
                         </h2>
-                        <h2 className="text-yellow-400 font-bold text-2xl 2xl:text-6xl xl:text-6xl lg:text-5xl md:text-4xl sm:3xl">
+                        <h2 className="text-yellow-400 font-bold text-2xl 2xl:text-6xl xl:text-6xl lg:text-5xl md:text-4xl sm:3xl max-sm:text-center">
                             BOCADILLO DE TERNERA
                         </h2>
                     </div>
@@ -25,10 +25,10 @@ export default function Home() {
                     </span>
                 </div>
                 <div className="flex flex-col justify-center items-center mt-8">
-                    <h2 className="font-bold text-md 2xl:text-4xl xl:text-3xl lg:text-2xl py-3 px-16 md:text-xl sm:lg border-b border-yellow-400">
+                    <h2 className="font-bold text-4xl text-center max-sm:text-xl py-3 px-16 border-b border-yellow-400">
                         Productos destacados
                     </h2>
-                    <div className="grid grid-cols-3 gap-2 w-full py-6 items-center justify-center px-2">
+                    <div className="grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-2 w-full py-6 items-center justify-center px-2">
                         <div className="flex flex-col items-center justify-between gap-3 h-full">
                             <Image alt="Bocadillo de Jamón" src={jamon} width={350} height={350} />
                             <h2 className="text-sm 2xl:text-xl xl:text-lg lg:text-md font-medium text-center">
@@ -52,10 +52,15 @@ export default function Home() {
             </main>
             <footer className="bg-yellow-500 p-5 flex justify-center items-center gap-10 mt-8">
                 <div className="flex flex-col gap-2">
-                    <h1 className="text-2xl font-bold text-center">MDK Burguer</h1>
-                    <span className="text-center">Calle Fenix Nº1, Madrid</span>
+                    <h1 className="text-md 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:lg font-bold text-center">
+                        MDK Burguer
+                    </h1>
+                    <span className="text-center md:text-md text-sm font-medium">
+                        Calle Fenix Nº1, Madrid
+                    </span>
+                    <span className="text-center text-sm">© 2024</span>
                 </div>
             </footer>
-        </>
+        </div>
     );
 }
